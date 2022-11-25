2cd43b4684189fc0ad1ec4078b738d844acf4365
gawbsouza <gabriel.souza@zoho.com>
2022-11-25 08:42:59 -0300

    fix: editions JSON name

--- app/book/book.go
+++ app/book/book.go
@@ -5,7 +5,7 @@ type Book struct {
 	Title    string   `json:"title" firestore:"title" binding:"required,gt=0"`
 	Authors  []string `json:"authors" firestore:"authors" binding:"required,gt=0"`
 	Year     int      `json:"year" firestore:"year" binding:"required,gt=0"`
-	Editions int      `json:"edition" firestore:"edition" binding:"required,gt=0"`
+	Editions int      `json:"editions" firestore:"editions" binding:"required,gt=0"`
 	Category string   `json:"category" firestore:"category" binding:"required,gt=0"`
 }
 
