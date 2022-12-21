ef2c6fb0be8b8af9307c388e4e998c92ef2dfaaf
gawbsouza <gabriel.souza@zoho.com>
2022-12-21 08:03:19 -0300

    Create entity layer

---
+++ app/entity/book.go
@@ -0,0 +1,10 @@
+package entity
+
+type Book struct {
+	ID      string
+	Title   string
+	Authors []string
+	Year    int
+	Edition int
+	Pages   int
+}
---
+++ app/entity/library.go
@@ -0,0 +1,5 @@
+package entity
+
+type Library struct {
+	Books []*Book
+}
